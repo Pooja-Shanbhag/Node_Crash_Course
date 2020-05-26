bc8e306fdfa0b11b086f146435ece1d1c7f0aa31
poojashanbhag <pooja.shanbhag1993@gmail.com>
2020-05-26 16:38:41 -0400

    Update fs_demo.js

--- references/fs_demo.js
+++ references/fs_demo.js
@@ -2,7 +2,28 @@ const fs = require('fs');
 const path = require('path');
 
 // Create a folder
-fs.mkdir(path.join(__dirname, '/test'), {}, (err) => {
-  if (err) throw err;
-  console.log('folder created');
-});
+// fs.mkdir(path.join(__dirname, '/test'), {}, (err) => {
+//   if (err) throw err;
+//   console.log('folder created');
+// });
+
+// Create a file
+fs.writeFile(
+  path.join(__dirname, '/test', 'hello.txt'),
+  'Hello World',
+  (err) => {
+    if (err) throw err;
+    console.log('file written to..');
+
+    fs.appendFile(
+      path.join(__dirname, '/test', 'hello.txt'),
+      'Node js Love',
+      (err) => {
+        if (err) throw err;
+        console.log('file written to..');
+      }
+    );
+  }
+);
+
+//Read file
