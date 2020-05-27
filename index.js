0fc682bcb29b4b8bbfb1055c199d520172f05461
poojashanbhag <pooja.shanbhag1993@gmail.com>
2020-05-27 19:28:46 -0400

    update

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const Person = require('./person');
+// const Person = require('./person');
 
-const person1 = new Person('John Doe', 30);
+// const person1 = new Person('John Doe', 30);
 
-console.log(person1.greeting());
+// console.log(person1.greeting());
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+logger.on('message', (data) => console.log('Called Listner: ', data));
+
+logger.log('Hello world');
+logger.log('Hello world2');
+logger.log('Hello world3');
