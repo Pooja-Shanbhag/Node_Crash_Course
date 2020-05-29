c729e32e75cf6decdf33520c73ae1949e0fd4115
poojashanbhag <pooja.shanbhag1993@gmail.com>
2020-05-28 21:11:22 -0400

    nodemon update

--- index.js
+++ index.js
@@ -4,11 +4,17 @@
 
 // console.log(person1.greeting());
 
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
-logger.on('message', (data) => console.log('Called Listner: ', data));
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  if (req.url === '/') {
+    res.end('<h1>Home</h1>');
+  }
+});
 
-logger.log('Hello world');
-logger.log('Hello world2');
-logger.log('Hello world3');
+// look for env variable, if not found run on 5000
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
